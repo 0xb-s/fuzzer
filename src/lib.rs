d53881061afb7a0ffb9366cebeb8a9c3c31bb2d8
0xb-s <145866191+0xb-s@users.noreply.github.com>
2024-10-04 16:16:59 -0700

    Create lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+pub mod allocator;
+pub mod analysis;
+pub mod config;
+pub mod errors;
+pub mod fuzz_engine;
+pub mod input;
+pub mod mutator;
+pub mod mutator_options;
+pub mod target;
+pub mod utils;
+pub use crate::config::FuzzerConfig;
+pub use crate::errors::FuzzerError;
+pub use crate::fuzz_engine::Fuzzer;
+pub use crate::target::{TargetFunction, TargetFunctionType};
