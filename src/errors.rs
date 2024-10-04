820320d32061bec31351e03dd730a9087ea95923
0xb-s <145866191+0xb-s@users.noreply.github.com>
2024-10-04 16:18:15 -0700

    Create errors.rs

---
+++ src/errors.rs
@@ -0,0 +1,20 @@
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+#[derive(Error, Debug, Deserialize, Serialize)]
+pub enum FuzzerError {
+    #[error("Input generation error: {0}")]
+    InputGenerationError(String),
+
+    #[error("Mutation error: {0}")]
+    MutationError(String),
+
+    #[error("Execution error: {0}")]
+    ExecutionError(String),
+
+    #[error("Timeout occurred")]
+    TimeoutError,
+
+    #[error("Custom error: {0}")]
+    CustomError(String),
+}
