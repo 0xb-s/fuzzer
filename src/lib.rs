ba2371193a004a21360fb86409a74eb224e6205e
0xb-s <145866191+0xb-s@users.noreply.github.com>
2024-11-06 04:40:08 -0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,17 @@
 pub mod allocator;
 pub mod analysis;
 pub mod config;
+pub mod coverage;
+pub mod crash_analysis;
+pub mod distributed;
 pub mod errors;
 pub mod fuzz_engine;
 pub mod input;
+pub mod logger;
 pub mod mutator;
 pub mod mutator_options;
+pub mod plugin;
+pub mod reproducer;
 pub mod target;
 pub mod utils;
 pub use crate::config::FuzzerConfig;
